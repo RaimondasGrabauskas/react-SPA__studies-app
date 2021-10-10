e0aa0f20d3c41563f6fb56ec4bd1ac2c5176039f
Raimis <rggrabauskas@gmail.com>
2021-10-10 09:13:25 +0300

    change title text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({onToggleList, onShowList}) => {
   return (
   <header className={css.header}>
     <div className={css['header-item']}>
-      <h2>Wanna see graduated studies press button</h2>
+      <h2>To see graduated studies press button</h2>
       <button onClick={onToggleList}>{onShowList ? 'Hide list' : 'Show list'}</button>
     </div>
   </header>
